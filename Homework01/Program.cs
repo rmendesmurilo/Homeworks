8cfcf61bd85d419a0d70cd1feba96e1e6de2af61
rmendesmurilo <mokrzzq@icloud.com>
2025-09-07 11:07:17 -0300

    Add files via upload

---
+++ Homework01/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Fred is excited about starting his program at NBCC");
+Console.WriteLine("IT: Software Development is the program Fred entered in the Fall of 2025.");
+Console.WriteLine("Programming Fundamentals is going to be the course that Fred likes best");
+Console.WriteLine("Fred is going to study very hard.");
+Console.WriteLine("Each night, Fred is going to spend time reviewing all of the course material.");
+Console.WriteLine("Fred will be committed to learning as much as possible about C# in 2025 and beyond!");
+
